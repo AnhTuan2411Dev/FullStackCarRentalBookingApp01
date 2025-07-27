6cf4c4440d02408c44052089d5ebb1f3b7b9f70e
AnhTuan2411Dev <anhtuan2411.dev@gmail.com>
2025-07-28 01:32:01 +0700

    Tạo Navigation Bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
     </>
   )
 }
